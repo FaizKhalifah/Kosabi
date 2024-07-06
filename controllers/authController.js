297d219e9b0b28bf22949be7995b37d07d9b6d9e
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-07-06 13:16:06 +0700

    feat : fungsi login

--- controllers/authController.js
+++ controllers/authController.js
@@ -59,9 +59,24 @@ async function register_post(req,res){
 }
 
 async function login_post(req,res){
+    const { email, password } = req.body;
 
+    try {
+      const user = await User.login(email, password);
+      const token = createToken(user._id);
+      res.cookie('jwt', token, { httpOnly: true, maxAge: maxAge * 1000 });
+      res.status(200).json({ user: user._id });
+    } 
+    catch (err) {
+      const errors = handleErrors(err);
+      res.status(400).json({ errors });
+    }
 }
 
+async function logout_get(req,res){
+    res.cookie('jwt', '', { maxAge: 1 });
+    res.redirect('/');
+}
 export default{
-    register_get,register_post,login_get,login_post
+    register_get,register_post,login_get,login_post,logout_get
 }
