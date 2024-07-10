316a3ac9d5dfdacba6fc2ba1d951528ccc658e57
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2024-07-11 06:32:01 +0700

    feat : room controller

---
+++ controllers/roomController.js
@@ -0,0 +1,69 @@
+import Room from "../models/room.js";
+
+async function index(req,res){
+    try{
+        const rooms = await Room.find();
+        let message = '';
+        if(rooms.length===0){
+            message="no room available";
+        }
+        res.render('admin/rooms/index',{rooms,message});
+    }catch(err){
+        res.status(500).send(err);
+    }
+}
+
+async function create(req,res){
+    res.render('admin/rooms/create');
+}
+
+async function store(req,res){
+    try{
+        const room = new Room(req.body);
+        await room.save();
+        res.redirect('/admin/rooms/index');
+    }catch(err){
+        res.status(500).send(err);
+    }
+}
+
+async function edit(req,res){
+    try{
+        const room = await Room.findById(req.params.id);
+        if(!room){
+            res.status(400).send('room not found');
+        }else{
+            res.render('admin/rooms/edit',{room});
+        }
+    }catch(err){
+        res.status(500).send(err);
+    }
+}
+
+async function update(){
+    try{
+        const room = await Room.findByIdAndUpdate(req.params.id, req.body);
+        if (!room) {
+            return res.status(404).send('Room not found');
+        }
+        res.redirect('admin/rooms/index');
+    }catch(err){
+        res.status(500).send(err);
+    }
+}
+
+async function destroy(){
+    try {
+        const room = await Room.findByIdAndDelete(req.params.id);
+        if (!room) {
+            return res.status(404).send('Room not found');
+        }
+        res.redirect('admin/rooms/delete');
+    } catch (err) {
+        res.status(500).send(err);
+    }
+}
+
+export default{
+    index,create,store,edit,update,destroy
+}
