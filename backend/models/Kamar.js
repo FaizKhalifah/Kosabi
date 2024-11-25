c7873e7d24adc2a79369ae935093b890d04fe4e9
Faiz Khalifah <faiz_doc@outlook.com>
2024-11-25 22:49:09 +0700

    feat: kamar routes

--- backend/models/Kamar.js
+++ backend/models/Kamar.js
@@ -1,30 +1,24 @@
-// models/Kamar.js
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('./index');
-
-class Kamar extends Model {}
-
-Kamar.init(
-  {
-    nomor_kamar: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      unique: true,
-    },
-    harga: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-    },
-    status: {
-      type: DataTypes.ENUM('tersedia', 'terisi'),
-      defaultValue: 'tersedia',
-    },
-  },
-  {
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Kamar extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      Kamar.hasOne(models.User, { foreignKey: 'kamarId', as: 'penyewa' });
+    }
+  }
+  Kamar.init({
+    nomor: DataTypes.STRING,
+    status: DataTypes.STRING
+  }, {
     sequelize,
     modelName: 'Kamar',
-    tableName: 'kamars',
-  }
-);
-
-module.exports = Kamar;
+  });
+  return Kamar;
+};
