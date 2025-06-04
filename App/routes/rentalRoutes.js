4f3768c798f5c43535f06192098ea48a42ebcc39
Faiz Khalifah <faiz_doc@outlook.com>
2025-06-04 19:45:36 +0700

    feat: add middleware to routes

--- App/routes/rentalRoutes.js
+++ App/routes/rentalRoutes.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const rentalController = require("../controllers/rentalController");
-
+const authMiddleware = require('../middleware/authMiddleware'); // Assuming you have an auth middleware
 const rentalRouter = express.Router();
-rentalRouter.get('/rentals', rentalController.index);
-rentalRouter.get('/rentals/add', rentalController.add);
-rentalRouter.post('/rentals/add', rentalController.store);
-rentalRouter.get('/rentals/:id/edit', rentalController.edit);   
-rentalRouter.post('/rentals/:id/edit', rentalController.update);
-rentalRouter.post('/rentals/:id/delete', rentalController.delete);
+rentalRouter.get('/rentals',authMiddleware.isAuthenticated, rentalController.index);
+rentalRouter.get('/rentals/add', authMiddleware.isAuthenticated,rentalController.add);
+rentalRouter.post('/rentals/add',authMiddleware.isAuthenticated, rentalController.store);
+rentalRouter.get('/rentals/:id/edit',authMiddleware.isAuthenticated, rentalController.edit);   
+rentalRouter.post('/rentals/:id/edit',authMiddleware.isAuthenticated, rentalController.update);
+rentalRouter.post('/rentals/:id/delete',authMiddleware.isAuthenticated, rentalController.delete);
 
 module.exports = rentalRouter;
--- App/routes/roomRoutes.js
+++ App/routes/roomRoutes.js
@@ -1,14 +1,14 @@
 const express = require('express');
 const roomController = require("../controllers/roomController");
 const upload = require("../middleware/imageUpload");
-
+const authMiddleware = require('../middleware/authMiddleware');
 const roomRouter = express.Router();
 
-roomRouter.get('/rooms',roomController.index);
-roomRouter.get('/rooms/add',roomController.add);
-roomRouter.post('/rooms/add',upload.single('photo'),roomController.store);
-roomRouter.get('/rooms/:id/edit',roomController.edit);
-roomRouter.post('/rooms/:id/edit',upload.single('photo'),roomController.update);
-roomRouter.post('/rooms/:id/delete',roomController.delete);
+roomRouter.get('/rooms',authMiddleware.isAuthenticated,roomController.index);
+roomRouter.get('/rooms/add',authMiddleware.isAuthenticated,roomController.add);
+roomRouter.post('/rooms/add',authMiddleware.isAuthenticated,upload.single('photo'),roomController.store);
+roomRouter.get('/rooms/:id/edit',authMiddleware.isAuthenticated,roomController.edit);
+roomRouter.post('/rooms/:id/edit',authMiddleware.isAuthenticated,upload.single('photo'),roomController.update);
+roomRouter.post('/rooms/:id/delete',authMiddleware.isAuthenticated,roomController.delete);
 
 module.exports = roomRouter;
