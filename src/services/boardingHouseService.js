d5ce78a6f7e549b1289f0487ed78d84264c3e808
Muhammad Alfaiz Khalifah Alamsyah <faiz_doc@outlook.com>
2026-09-14 11:00:38 +0700

    feat: update and delete boarding house service

--- src/services/boardingHouseService.js
+++ src/services/boardingHouseService.js
@@ -66,4 +66,72 @@ export default class BoardingHouseService {
       photo: photopath,
     });
   }
+
+  async updateBoardingHouse(id, data, photopath) {
+    if (!id) {
+      throw new Error("Id is required to update boarding house");
+    }
+
+    const boardingHouse = await this.repository.findById(id);
+    if (!boardingHouse) {
+      throw new Error("Boarding house not found");
+    }
+
+    if (photopath && boardingHouse.photo) {
+      const oldPath = path.join("public", boardingHouse.photo);
+      if (fs.existsSync(oldPath)) {
+        fs.unlinkSync(oldPath);
+      }
+    }
+
+    const {
+      name,
+      description,
+      address,
+      city,
+      province,
+      postalCode,
+      email,
+      phone,
+      rules,
+      facilities,
+      location,
+      checkInTime,
+      checkOutTime,
+      status,
+    } = data;
+
+    return await this.repository.update({
+      name,
+      description,
+      address,
+      city,
+      province,
+      postalCode,
+      email,
+      phone,
+      rules,
+      facilities,
+      location,
+      checkInTime,
+      checkOutTime,
+      status,
+      photo: photopath,
+    });
+  }
+
+  async deleteBoardingHouse(id) {
+    if (!id) {
+      throw new Error("Id is required to delete boarding house");
+    }
+
+    const boardingHouse = await this.repository.findById(id);
+    if (!boardingHouse) {
+      throw new Error("Boarding house not found");
+    }
+    await this.repository.delete(id);
+    return {
+      message: "BoardingHouse has been deleted",
+    };
+  }
 }
